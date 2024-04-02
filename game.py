58ef138a6fab93dfe132934224e3295e505d3163
Rajarshee Das <rajarsheedas14@gmail.com>
2024-04-02 17:16:02 +0530

    Main file added

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Naughty Kids")
+
+
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit() 
