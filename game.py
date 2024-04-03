71b9937d9a51c18547d0fdfbca033a75d7a75b21
Rajarshee Das <rajarsheedas14@gmail.com>
2024-04-03 20:50:37 +0530

    changes

--- game.py
+++ game.py
@@ -3,8 +3,8 @@ import sys
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
+screen_width = 1504
+screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Naughty Kids")
 
