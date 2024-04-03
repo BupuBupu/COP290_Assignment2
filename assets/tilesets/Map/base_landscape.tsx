01f210728e83ab941fcd2df91531db017f407520
Rajarshee Das <rajarsheedas14@gmail.com>
2024-04-03 22:15:08 +0530

    Map added using pytmx

---
+++ assets/tilesets/Map/base_landscape.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="base_landscape" tilewidth="16" tileheight="16" tilecount="468" columns="18">
+ <image source="../base_landscape.png" width="288" height="416"/>
+</tileset>
--- game.py
+++ game.py
@@ -1,13 +1,32 @@
 import pygame
 import sys
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+
+from pytmx.util_pygame import load_pygame
+
 pygame.init()
 
 screen_width = 1504
 screen_height = 768
 screen = pygame.display.set_mode((screen_width, screen_height))
+tmx_data = load_pygame("./assets/tilesets/Map/Map.tmx")
 pygame.display.set_caption("Naughty Kids")
+sprite_group = pygame.sprite.Group()
 
+
+
+# cycle through layers
+
+for layer in tmx_data.layers:
+    if hasattr(layer, 'data'):
+        for x,y,surf in layer.tiles():
+            pos = (x*16, y*16)
+            Tile(pos,surf,sprite_group)
 
 game_over = False
 
@@ -16,6 +35,7 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
 
+    sprite_group.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
