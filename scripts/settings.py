1458105eb9f96f0185eab5e38ed4465c6d3878c9
Vipul-Vaibhav27 <cs1221301@iitd.ac.in>
2024-04-07 14:00:29 +0530

    Added enemy entity and created fake 3D effect and modularise some attributes

--- scripts/settings.py
+++ scripts/settings.py
@@ -3,3 +3,13 @@ from pygame.math import Vector2
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 TILE_SIZE = 64
+
+# player attributes
+PLAYER_SPEED = 200
+PLAYER_ANIMATION_SPEED = 4
+
+# lower the layer num, more below it is in z-axis
+LAYERS = {
+    "enemy": 0,
+    "player": 0
+}
