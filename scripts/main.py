2204b6a322cec1f7a7539cf0af99424b6fb8278c
Rajarshee Das <rajarsheedas14@gmail.com>
2024-04-15 23:07:55 +0530

    main menu added

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 import pygame, sys, time
 from settings import *
 from level import Level
+import button
 
 class Game:
 	def __init__(self):
@@ -9,24 +10,44 @@ class Game:
 		pygame.display.set_caption('Sprout land')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
+		self.game_paused = True
+		self.game_menu = "main_menu"
+		self.start_image = pygame.image.load("./assets/UIs/Main_Menu/start_btn.png").convert_alpha()
+		self.exit_image = pygame.image.load("./assets/UIs/Main_Menu/exit_btn.png").convert_alpha()
+		self.start_btn = button.Button(400, 300, self.start_image, 1)
+		self.exit_btn = button.Button(850, 300, self.exit_image, 1)
 
 	def run(self):
 		pygame.mouse.set_visible(False)
 		self.clock.tick(60)
 		previous_time = time.time()
 		while True:
+			if self.game_paused:
+				pygame.mouse.set_visible(True)
+				self.screen.fill((52,78,91))
+				if self.game_menu == "main_menu":
+					if self.start_btn.draw(self.screen):
+						self.game_menu = "playing"
+						self.game_paused = False
+						self.level = Level()
+					if self.exit_btn.draw(self.screen):
+						pygame.quit()
+						sys.exit()
+				pygame.display.update()
+			
 			dt = time.time() - previous_time
 			previous_time = time.time()
 			keys = pygame.key.get_pressed()
 			if keys[pygame.K_ESCAPE]:
-				pygame.quit()
-				sys.exit()
+				self.game_paused = True
+				self.game_menu = "main_menu"
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
-
-			self.level.run(dt)
+			if not self.game_paused:
+				self.screen.fill((202, 228, 241))
+				self.level.run(dt)
 			pygame.display.update()
 
 if __name__ == '__main__':
